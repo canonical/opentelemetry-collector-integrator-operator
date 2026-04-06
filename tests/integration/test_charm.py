1d944c4fddc01226fc3ad26bb8a93ca20c6ef79d
Jose C. Massón <jose.masson@canonical.com>
2026-04-06 15:10:06 -0300

    remove wrong test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -1,4 +1,4 @@
-# Copyright 2026 jose
+# Copyright 2026 Canonical Ltd.
 # See LICENSE file for licensing details.
 #
 # The integration tests use the Jubilant library. See https://documentation.ubuntu.com/jubilant/
@@ -8,28 +8,11 @@ import logging
 import pathlib
 
 import jubilant
-import pytest
-import yaml
 
 logger = logging.getLogger(__name__)
-
-METADATA = yaml.safe_load(pathlib.Path("charmcraft.yaml").read_text())
 
 
 def test_deploy(charm: pathlib.Path, juju: jubilant.Juju):
     """Deploy the charm under test."""
-    resources = {
-        "some-container-image": METADATA["resources"]["some-container-image"]["upstream-source"]
-    }
-    juju.deploy(charm.resolve(), app="opentelemetry-collector-integrator-operator", resources=resources)
-    juju.wait(jubilant.all_active)
-
-
-# If you implement opentelemetry_collector_integrator.get_version in the charm source,
-# remove the @pytest.mark.skip line to enable this test.
-# Alternatively, remove this test if you don't need it.
-@pytest.mark.skip(reason="opentelemetry_collector_integrator.get_version is not implemented")
-def test_workload_version_is_set(charm: pathlib.Path, juju: jubilant.Juju):
-    """Check that the correct version of the workload is running."""
-    version = juju.status().apps["opentelemetry-collector-integrator-operator"].version
-    assert version == "3.14"  # Replace 3.14 by the expected version of the workload.
+    juju.deploy(charm.resolve(), app="opentelemetry-collector-integrator-operator")
+    juju.wait(jubilant.all_blocked)
